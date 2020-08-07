68aa0835c355aa6e1940c624e04be48e762b03e1
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-08-07 11:03:52 +0200

    Application terminé

--- TourGuide2_microService_Zuul/src/main/java/com/tourguide/TourGuide2MicroServiceZuulApplication.java
+++ TourGuide2_microService_Zuul/src/main/java/com/tourguide/TourGuide2MicroServiceZuulApplication.java
@@ -3,15 +3,23 @@ package com.tourguide;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class TourGuide2MicroServiceZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TourGuide2MicroServiceZuulApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes().route(r -> r.path("/user/**").uri("lb://MICROSERVICE-USER").id("microservice-user"))
+				.route(r -> r.path("/rewards/**").uri("lb://MICROSERVICE-REWARD").id("microservice-reward"))
+				.route(r -> r.path("/gpsutil/**").uri("lb://MICROSERVICE-GPSUTIL").id("microservice-gpsutil")).build();
 	}
 
 }
